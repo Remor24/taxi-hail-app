37e882bf6ece21323c984eacd8a014c1f90eebdd
Remor <remor.candelaria@gmail.com>
2025-05-24 16:24:33 +0800

    Fix node_modules and regenerate lockfile

--- taxi-hail-app/client/src/App.js
+++ taxi-hail-app/client/src/App.js
@@ -1,1 +1,11 @@
-// Main App routing logic (to be implemented)
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1>Hello Taxi Hail App!</h1>
+    </div>
+  );
+}
+
+export default App;
